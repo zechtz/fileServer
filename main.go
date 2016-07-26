9b18efb57050c74513773bb2a2597b7ff17345ca
Zachariah Ngonyani <zachariahn@gmail.com>
2016-07-26 22:46:06 +0300

    Add procfile and .godir file

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"github.com/russross/blackfriday"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/markdown", GenerateMarkdown)
 	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 }
 
 func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
