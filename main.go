b149380dd857af3a5ab2598bfafbb7403fcfb448
Zachariah Ngonyani <zachariahn@gmail.com>
2016-07-26 22:40:06 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/russross/blackfriday"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8080", nil)
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
+}
